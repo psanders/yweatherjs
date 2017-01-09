62fac572daf371b16a876c9e0a647a715f967cae
Pedro Sanders <sanderspedro@gmail.com>
2017-01-08 22:05:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-#!/usr/bin/jjs -fv -scripting -cp ./lib/httpcore-4.4.4.jar:./lib/commons-logging-1.2.jar:./lib/commons-codec-1.9.jar:./lib/commons-httpclient-3.1.jar
+#!/usr/bin/jjs -cp ./lib/httpcore-4.4.4.jar:./lib/commons-logging-1.2.jar:./lib/commons-codec-1.9.jar:./lib/commons-httpclient-3.1.jar
 
 var HttpClient = Java.type('org.apache.commons.httpclient.HttpClient');
 var GetMethod = Java.type('org.apache.commons.httpclient.methods.GetMethod');
